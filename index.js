568610554f770fc1a3824db9db0692fae0feb33f
wessan001 <wesleyortegassan@gmail.com>
2023-06-16 12:29:51 -0300

    Chatbot for Telegram with JS and NodeJS

---
+++ index.js
@@ -0,0 +1,23 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '6080384530:AAGC3WWF7Vl77Y_L5Dcukb-wFuCoOwP9SBM';
+
+const bot = new TelegramBot(token, {polling:true});
+
+bot.on('message', function (msg) {
+    const chatId = msg.chat.id;
+    console.log(msg.text);
+    bot.sendMessage(chatId, 'Quer entrar no grupo?');
+});
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
